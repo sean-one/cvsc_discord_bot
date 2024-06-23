1e2706b5579d45ccaeb9c7c2ed890ab71bcf4421
sean-one <basement365@gmail.com>
2024-06-22 22:38:35 -0700

    working on ping

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token: TOKEN } = require('./config.json');
+const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
+const { token } = require('./config.json');
+const fs = require('fs');
+const path = require('path');
 
 // create a new client
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessageReactions] });
 
-client.once(Events.ClientReady, readyClient => {
-    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-})
+// collection for commands
+client.commands = new Collection();
 
-client.login(TOKEN)
+// Reading command files
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    client.commands.set(command.data.name, command);
+}
+
+// Reading event files
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+client.login(token)
